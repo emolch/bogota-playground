1ea2c0c1ebaf757496d2e50fb0b4c19119094279
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2016-10-04 11:12:51 +0200

    let rapidinv be independent of kiwi tunguska python modules

---
+++ modules/tunguska/phase.py
@@ -0,0 +1,181 @@
+import util
+from bisect import bisect
+import sys, os
+
+class OutOfBounds(Exception):
+    pass
+
+class PLF:
+    '''Nestable piecewise linear function'''
+    
+    def __init__(self, xdata, ydata):
+        self.xdata = xdata
+        self.ydata = ydata
+        
+    def __call__(self, *args):
+        x = args[0]
+        y0, y1, frac = self.ip(x)
+        if isinstance(y0,PLF):
+            y0 = y0(*args[1:])
+        if isinstance(y1,PLF):
+            y1 = y1(*args[1:])
+        return y0 + frac*(y1-y0)
+        
+    def ip(self,x):
+        xdata = self.xdata
+        ydata = self.ydata
+        if x < xdata[0]: raise OutOfBounds()
+        if x > xdata[-1]: raise OutOfBounds()
+        i = bisect(xdata, x)
+        i = max(1, i)
+        i = min(len(xdata)-1, i)
+        frac = (x-xdata[i-1])/(xdata[i]-xdata[i-1])
+        return ydata[i-1], ydata[i], frac
+        
+class Phase:
+    def __init__(self,name,filename=None):
+    
+        self.name = name
+        self.filename = filename
+        if filename is None:
+            if os.path.isfile(name+'.phase'):
+                filename = name+'.phase'
+            else:
+                filename = os.path.join(util.kiwi_aux_dir(), 'phases', name)
+            
+        
+        f = open(filename,'r')
+        self.ref_points = []
+        dists = {}
+        distances, depths, times = [], [], []
+        have_seen = {}
+        have_depth = False
+        for line in f:
+            toks = line.split()
+            dist = float(toks[0])
+            if len(toks) == 3:
+                depth = float(toks[1])
+                have_depth = True
+            else:
+                depth = 10000.
+            
+            if (dist,depth) not in have_seen:
+                times.append(float(toks[-1]))
+                distances.append(dist)
+                depths.append(depth)
+            
+            have_seen[(dist,depth)] = True
+            
+        f.close()
+        if have_depth:
+            dists = {}
+            for di, de, ti in zip(distances, depths, times):
+                if not di in dists:
+                    dists[di] = ([], [])
+                dists[di][0].append(de)
+                dists[di][1].append(ti)
+            distances1 = []
+            depth_plfs = []
+            for (di, (des,tis)) in sorted(dists.items()):
+                distances1.append(di)
+                depth_plfs.append( PLF(des,tis) )
+            self.lookup = PLF(distances1, depth_plfs)
+        else:
+            self.lookup = PLF(distances, times)
+            
+        self.have_depth = have_depth
+                
+    def __call__(self, distance, depth=10000.):
+        distance = float(distance)
+        depth = float(depth)
+        try:
+            return self.lookup(distance, depth)
+        except OutOfBounds:
+            return None
+    
+    def __repr__(self):
+        s = "Phase(name='%s'" % self.name
+        if self.filename is not None:
+            s += ", filename='%s'" % self.filename
+        s += ')'
+        return s
+        
+            
+class Timing:
+    '''"Intelligent" Phase, e.g. "S or Sn, whatever is available minus 10 s".'''
+    
+    def __init__(self, phases, offset=0.):
+        '''usage:
+           Timing('S')
+           Timing(('S','Sn'),+5)
+           Timing(Phase('R'),-10)'''
+        
+        if isinstance(phases, str):
+            phases = [ phases ]
+        self.phases = []
+        for p in phases:
+            if isinstance(p, str):
+                self.phases.append( Phase(p) )
+            else:
+                self.phases.append( p )
+                
+        self.offset = float(offset)
+        
+    def __call__(self, distance, depth=10000.):
+        '''Returns timing at given distance.'''
+        for phase in self.phases:
+            t = phase(distance, depth)
+            if not t is None:
+                return t+self.offset
+
+    def __repr__(self):
+        s = 'Timing(phases=['
+        s += ', '.join([repr(p) for p in self.phases])
+        s += '], offset='+str(self.offset)+')'
+        return s
+        
+class Taper:
+    def __init__(self, timings=None,
+                       phases=None, offsets=None, amplitude=1., amplitudes=None):
+        '''usage:
+           t = Taper( timings=(Timing('P',-10),Timing('P', 0), ...)
+           t = Taper( phases=('S','Sn'), offsets=(-10,0,40,50) )'''
+        
+        if phases and offsets:
+            timings = [ Timing(phases,offset) for offset in offsets ]
+            
+        assert(len(timings) == 4)
+        self.timings = timings
+
+        if amplitudes is not None:
+            assert len(amplitudes) == 4
+            self.amplitudes = amplitudes[:]
+        else:
+            self.amplitudes = [ 0., amplitude, amplitude, 0. ]
+        
+    def __call__(self, distance, depth=10000.):
+        '''Returns representation which can be used for Seismosizer.do_set_misfit_taper().'''
+        return ( self.timings[0](distance,depth), self.amplitudes[0],
+                 self.timings[1](distance,depth), self.amplitudes[1],
+                 self.timings[2](distance,depth), self.amplitudes[2],
+                 self.timings[3](distance,depth), self.amplitudes[3])
+
+    def __repr__(self):
+        s = 'Taper(timings=[\n    '
+        s += ',\n    '.join([repr(t) for t in self.timings])
+        s += '\n])'
+        return s
+        
+if __name__ == '__main__':
+    p = Phase('P')
+    pn = Phase('Pn')
+    for i in range(15):
+        print p(i*1000000.), pn(i*1000000.)
+
+    sany = ('S','Sn')
+    timings = [ Timing(sany,offset) for offset in [-10., 0., 40., 50. ] ] 
+    print Taper(timings)(1000000.)
+    t = Taper(phases=('S','Sn'), offsets=(-10,0, 40,50))
+    print t(1000000.)
+    print repr( t )
+
---
+++ modules/tunguska/util.py
@@ -0,0 +1,174 @@
+
+import os
+import subprocess
+import tempfile
+
+import pickle
+import shutil
+import sys
+import numpy as num
+pjoin = os.path.join
+
+def kiwi_aux_dir():
+    ieq_home = os.getenv('KIWI_HOME')
+    if ieq_home is None:
+         sys.exit('KIWI_HOME environment variable not set')
+    d = pjoin(ieq_home, 'aux')
+    if not os.path.isdir(d):
+        sys.exit('directory not found: "%s"' % d)
+    return d
+
+def kiwi_aux_file(*p):
+    return pjoin(kiwi_aux_dir(), *p)
+
+def unindent(s):
+    lines = s.splitlines()
+    if lines and not lines[0].strip(): lines.pop(0)
+    if lines and not lines[-1].strip(): lines.pop()
+    minindent = min([ len(line) - len(line.lstrip()) for line in lines if line.strip() ])
+    eat = ' '*minindent
+    outlines = []
+    for line in lines:
+        if line.startswith(eat):
+            outlines.append(line[minindent:])
+        else:
+            outlines.append(line)
+    return '\n'.join(outlines)
+
+
+def gform( number, significant_digits=3 ):
+    '''Pretty print floating point numbers.
+    
+Align floating point numbers at the decimal dot.
+       
+      |  -d.dde+xxx|
+      |  -d.dde+xx |
+      |-ddd.       |
+      | -dd.d      |
+      |  -d.dd     |
+      |  -0.ddd    |
+      |  -0.0ddd   |
+      |  -0.00ddd  |
+      |  -d.dde-xx |
+      |  -d.dde-xxx|'''
+    
+    no_exp_range = (pow(10.,-1), 
+                    pow(10.,significant_digits))
+    width = significant_digits+significant_digits-1+1+1+5
+    
+    if (no_exp_range[0] <= abs(number) < no_exp_range[1]) or number == 0.:
+        s = ('%#.*g' % (significant_digits, number)).rstrip('0')
+    else:
+        s = '%.*E' % (significant_digits-1, number)
+    s = (' '*(-s.find('.')+(significant_digits+1))+s).ljust(width)
+    return s
+    
+    
+def gmt_color( rgb ):
+    return '%i/%i/%i' % rgb
+
+def autoplot( *args, **kwargs ):
+
+    outfile = args[-1]
+    things = args[:-1]
+    infiles = []
+    
+    if 'O' in kwargs:
+        mode = 'a'
+    else:
+        mode = 'w'
+        
+    f = open('%s.autoplot' % outfile, mode)
+    pickle.dump((args,kwargs), f)
+    f.close()
+    
+    argopts = None
+    if 'argopts' in kwargs:
+        argopts = kwargs.pop('argopts')
+        assert(len(argopts) == len(things)) 
+    
+    tempdir = tempfile.mkdtemp("","autplot-")
+    
+    for ithing, thing in enumerate(things):
+                
+        if type(thing) is str:
+            infiles.append(str)
+            
+        elif isinstance(thing, num.ndarray):
+            tfn = pjoin(tempdir, 'plotdata-%i.table' % ithing)
+            num.savetxt(tfn, thing)
+            infiles.append(tfn)
+        
+        elif type(thing) is tuple or type(thing) is list:
+            
+            tfn = pjoin(tempdir, 'plotdata-%i.table' % ithing)
+                        
+            if thing[-1].ndim == 2 and len(thing) == 3:     # (x[:],y[:],z[:,:])
+                x,y,z = thing
+                nx = x.size
+                ny = y.size
+                assert( ny == z.shape[0] and nx == z.shape[1] )
+                nz = nx*ny
+                tab = num.zeros((nx*ny,3),dtype=num.float)
+                tab[:,2] = z.reshape((nz,))
+                tab[:,1] = y.repeat(nx)
+                for i in range(ny): tab[i*nx:i*nx+nx,0] = x
+                num.savetxt(tfn,tab)
+            else:
+                nrows = thing[0].size
+                ncols = len(thing)
+                tab = num.zeros((nrows,ncols), dtype=num.float)
+                for icol,col in enumerate(thing):
+                    assert(col.size == nrows)
+                    tab[:,icol] = col
+                num.savetxt(tfn,tab)
+            infiles.append(tfn)
+            
+        
+
+    options = []
+    for k,v in kwargs.items():
+        if type(v) is bool:
+            if v:
+                options.append( '--%s' % k )
+        elif type(v) is tuple or type(v) is list:
+            options.append( '--%s=' % k + '/'.join([ str(x) for x in v]) )
+        else:
+            options.append( '--%s=%s' % (k,str(v)) )
+    
+    args = [ 'autoplot' ]
+    
+    infiles_with_argopts = []
+    if argopts:
+        for infile, argopt in zip(infiles, argopts):
+            if argopt:
+                infiles_with_argopts.append( infile+'['+argopt+']' )
+            else:
+                infiles_with_argopts.append( infile )
+    else:
+        infiles_with_argopts.extend(infiles)
+    
+    args.extend( infiles_with_argopts )
+    args.append( outfile )
+    args.extend( options )
+    subprocess.call( args )
+    
+    shutil.rmtree(tempdir)
+
+def cmp_version(a,b):
+    ai = [ int(x) for x in a.split('.') ]
+    bi = [ int(x) for x in b.split('.') ]
+    return cmp(ai, bi)
+
+if __name__ == '__main__':
+    import random
+    for i in range(0,15):
+        n = 10**(i-7)
+        print '|%s|' % gform(n)
+    for i in range(0,15):
+        n = 10**(i-7)*(random.random()*2.-1.)
+        print '|%s|' % gform(n)
+    print '|%s|' % gform(0.)
+    print '|%s|' % gform(0.1)
+
+
